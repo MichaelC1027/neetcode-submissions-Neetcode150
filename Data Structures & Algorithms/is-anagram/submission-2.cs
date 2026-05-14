4f2a874d4a570673c8739eddfc3f184a04c22392
Michael Castillo <91919088+MichaelC1027@users.noreply.github.com>
2026-05-14 18:54:10 -0400

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if(s.Length != t.Length) return false;
+
+        char[] sArr = s.ToCharArray();
+        char[] tArr = t.ToCharArray();
+        Array.Sort(sArr);
+        Array.Sort(tArr);
+        return sArr.SequenceEqual(tArr);
+
+    }
+}
